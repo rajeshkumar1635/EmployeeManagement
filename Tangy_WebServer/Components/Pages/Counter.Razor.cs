ff1b91e7fe8fa486e13736414b2b07fa7f1776a1
rajeshkumar1635 <rajeshkumar.kurapati23@gmail.com>
2025-10-25 12:22:26 -0400

    Initial commit

---
+++ Tangy_WebServer/Components/Pages/Counter.Razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Tangy_WebServer.Components.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+        private string fontFamily = "Monotype Corsiva";
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
